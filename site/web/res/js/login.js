f5cffc75a46cdd50656e9f2d9c5d38348ab6f5ed
Joachim Berge <jfberge@stud.ntnu.no>
2024-09-22 21:38:29 +0200

    created login page, however, no functionality

---
+++ site/web/res/js/login.js
@@ -0,0 +1,10 @@
+
+    const loginButton = document.getElementById('login');
+    
+    loginButton.addEventListener('mouseover', function() {
+        loginButton.src = '../../../images/login_hover.png';
+    });
+
+    loginButton.addEventListener('mouseout', function() {
+        loginButton.src = '../../../images/login.png';
+    });
