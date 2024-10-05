1605a8269f9e683238ba5fec14a32aff8904a893
Bendik <bendir@stud.ntnu.no>
2024-10-05 10:23:43 +0200

    feat: Added GetBasisCanvasesByDateTime function

--- site/internal/database/basisCanvas.go
+++ site/internal/database/basisCanvas.go
@@ -5,7 +5,45 @@ import (
 	"errors"
 	"fmt"
 	"rapidart/internal/models"
+	"time"
 )
+
+// Fetches a list of BasisCanvases given a date and time. Fetches this via the BasisGallery table.
+func GetBasisCanvasesByDateTime(datetime time.Time) ([]models.BasisCanvas, error) {
+	var canvases []models.BasisCanvas
+
+	// Query DB
+	rows, err := db.Query(
+		"SELECT BC.BasisCanvasId, BC.BasisGalleryId, BC.Type, BC.Image "+
+			"FROM `BasisGallery` as BG "+
+			"INNER JOIN `BasisCanvas` as BC ON BG.BasisGalleryId = BC.BasisGalleryId "+
+			"WHERE ? BETWEEN BG.StartTimestamp AND BG.EndTimestamp",
+		datetime)
+	// General error
+	if err != nil {
+		return []models.BasisCanvas{}, err
+	}
+
+	defer rows.Close()
+
+	// Loop through rows
+	for rows.Next() {
+		canvases = append(canvases, models.BasisCanvas{})
+
+		err := rows.Scan(
+			&canvases[len(canvases)-1].BasisCanvasId,
+			&canvases[len(canvases)-1].BasisGalleryId,
+			&canvases[len(canvases)-1].Type,
+			&canvases[len(canvases)-1].Image,
+		)
+		// General error
+		if err != nil {
+			return []models.BasisCanvas{}, err
+		}
+	}
+
+	return canvases, nil
+}
 
 // Fetches BasisCanvas data based on the given ID
 func GetBasisCanvasById(id int) (models.BasisCanvas, error) {
