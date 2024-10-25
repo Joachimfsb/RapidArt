4a8c7d40df4cccd7e6cf0bc1e3eddbb81cf09455
Bendik <bendir@stud.ntnu.no>
2024-10-25 15:19:44 +0200

    feat: Improved routing to allow viewing of other peoples profiles

--- site/internal/handlers/web/Profile.go
+++ site/internal/handlers/web/Profile.go
@@ -5,47 +5,59 @@ import (
 	"net/http"
 	"rapidart/internal/auth"
 	"rapidart/internal/models"
+	"rapidart/internal/user"
 	"rapidart/internal/util"
 )
 
 // /////////////// TEMPLATE MODEL ///////////////////
 type profileTemplateModel struct {
-	User models.User
+	IsSelf bool // Is this the logged in users account?
+	User   models.User
 }
 
-///////////////// HANDLER //////////////////
-
+// /////////////// HANDLER //////////////////
 func Profile(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case http.MethodGet:
-		profileGetRequest(w, r)
-	default:
-		http.Error(w, "this method is not supported", http.StatusNotImplemented)
-	}
-}
 
-func profileGetRequest(w http.ResponseWriter, r *http.Request) {
+	var isSelf bool
+	var u models.User
 
-	// Get session cookie
-	cookie, err := r.Cookie("session-token")
-	if err != nil {
-		util.HttpReturnError(http.StatusUnauthorized, w)
-		return
-	}
+	username := r.PathValue("username")
+	if username == "" {
+		// Verdi er tom, hent innlogget bruker
+		isSelf = true
 
-	// Get currently logged in user
-	u, err := auth.GetLoggedInUser(cookie.Value)
-	if err != nil {
-		util.HttpReturnError(http.StatusUnauthorized, w)
-		return
+		// Get session cookie
+		cookie, err := r.Cookie("session-token")
+		if err != nil {
+			util.HttpReturnError(http.StatusUnauthorized, w)
+			return
+		}
+
+		// Get currently logged in user
+		u, err = auth.GetLoggedInUser(cookie.Value)
+		if err != nil {
+			util.HttpReturnError(http.StatusUnauthorized, w)
+			return
+		}
+	} else {
+		// Brukernavn spesifiert, hent brukerinfo
+		isSelf = false
+
+		var err error
+		u, err = user.GetUserByUsername(username)
+		if err != nil {
+			util.HttpReturnError(http.StatusBadRequest, w)
+			return
+		}
 	}
 
 	// Create model
 	model := profileTemplateModel{
-		User: u,
+		IsSelf: isSelf,
+		User:   u,
 	}
 
-	err = util.HttpServeTemplate("profile.tmpl", model, w)
+	err := util.HttpServeTemplate("profile.tmpl", model, w)
 	if err != nil {
 		log.Println(err)
 		util.HttpReturnError(http.StatusInternalServerError, w)
