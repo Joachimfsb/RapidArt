1ee3f6406f1f3b276f3cc352a573113571bcefc8
Joachim Berge <jfberge@stud.ntnu.no>
2024-10-29 13:12:48 +0100

    bug fixing

--- site/internal/database/commentAPI_test.go
+++ site/internal/database/commentAPI_test.go
@@ -39,14 +39,14 @@ func TestGetAllCommentsFromPost(t *testing.T) {
 	gallery := test.GenBasisGallery()
 	canvas := test.GenBasisCanvas(gallery.BasisGalleryId)
 	post, _ := test.GenTestPost(user.UserId, canvas.BasisCanvasId, false)
-	comment := test.GenReport(user.UserId, post.PostId)
+	comment := test.GenComment(user.UserId, post.PostId)
 
 	// Expect the INSERT query in NewReport
-	mock.ExpectExec(`^INSERT INTO Report`).WithArgs(comment.UserId, comment.PostId, comment.Message, comment.CreationDateTime).
+	mock.ExpectExec(`^INSERT INTO Comment`).WithArgs(comment.UserId, comment.PostId, comment.Message, comment.CreationDateTime).
 		WillReturnResult(sqlmock.NewResult(1, 1))
 
 	// Call NewReport to execute the INSERT expectation
-	if err := NewReport(comment); err != nil {
+	if err := AddCommentToPost(comment); err != nil {
 		t.Fatal("Error inserting report: " + err.Error())
 	}
 
@@ -54,17 +54,17 @@ func TestGetAllCommentsFromPost(t *testing.T) {
 	rows := sqlmock.NewRows([]string{"UserId", "PostId", "Message", "CreationDateTime"}).
 		AddRow(comment.UserId, comment.PostId, comment.Message, comment.CreationDateTime)
 
-	mock.ExpectQuery(`^SELECT \* FROM Report WHERE PostId = ?`).WithArgs(comment.PostId).WillReturnRows(rows)
+	mock.ExpectQuery(`^SELECT \* FROM Comment WHERE PostId = ?`).WithArgs(comment.PostId).WillReturnRows(rows)
 
 	// Call GetAllReportsForPost to execute the SELECT expectation
-	reportAmount, err := GetAllReportsForPost(comment.PostId)
+	comments, err := GetAllCommentsFromPost(comment.PostId)
 	if err != nil {
-		t.Fatal("Got error trying to count reports: " + err.Error())
+		t.Fatal("Got error trying to count comments: " + err.Error())
 	}
 
 	// Check that the returned report amount is as expected
-	if len(reportAmount) != 1 {
-		t.Fatal("Wrong amount of reports: expected 1, got", len(reportAmount))
+	if len(comments) != 1 {
+		t.Fatal("Wrong amount of reports: expected 1, got", len(comments))
 	}
 
 	// Ensure all expectations were met
