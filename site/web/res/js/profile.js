5fbf06fd34e5dc825e3bca8f1b4bb1dd543cbc05
Bendik <bendir@stud.ntnu.no>
2024-10-29 20:16:33 +0100

    Added share functionality if supported

--- site/web/res/js/profile.js
+++ site/web/res/js/profile.js
@@ -1,8 +1,36 @@
-
 window.addEventListener('load', function () {
+
     const logoutButton = document.querySelector('#logout');
 
-    logoutButton.addEventListener('click', function(e) {
-        logout(e);
-    });
+    if (logoutButton != null) {
+        logoutButton.addEventListener('click', function(e) {
+            logout(e);
+        });
+    }
+
+    
+      
+    const shareBtn = document.querySelector("button#share");
+    
+    // Share must be triggered by "user activation"
+    if (navigator.share) {
+        shareBtn.addEventListener("click", async () => {
+            const displayname = document.querySelector("#displayname").textContent;
+
+            const shareData = {
+                title: "RapidArt profile",
+                text: "View " + displayname + "'s profile",
+                url: this.window.location,
+            };
+
+            try {
+                await navigator.share(shareData);
+            } catch (err) {
+            }
+        });
+    } else {
+        // Hide share button
+        shareBtn.classList.add("hide");
+    }
+      
 });
