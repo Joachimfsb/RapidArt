b118c52d83748e4b54527fac6ad798976f94cfb5
Bendik <bendir@stud.ntnu.no>
2024-10-30 10:25:21 +0100

    Added coloring to nav buttons

--- site/web/res/js/common.js
+++ site/web/res/js/common.js
@@ -1,3 +1,22 @@
+window.addEventListener('load', function () {
+
+    ///////// HEADER ///////////
+    // Color header button based on what page we are on
+    let home = document.querySelector("#nav-home");
+    let search = document.querySelector("#nav-search");
+    let toplist = document.querySelector("#nav-toplist");
+    let profile = document.querySelector("#nav-profile");
+    
+    let path = window.location.pathname;
+
+    if (path == "/") home.classList.add("red-item");
+    else if (path.startsWith("/search")) search.classList.add("red-item");
+    else if (path.startsWith("/toplist")) toplist.classList.add("red-item");
+    else if (path.startsWith("/profile")) profile.classList.add("red-item");
+
+});
+
+
 // Sends an API request to log the user out
 function logout(e) {
     e.preventDefault();
